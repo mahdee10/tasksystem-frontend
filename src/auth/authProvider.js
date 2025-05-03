5d9092e8e8355fa29d97fea2a129294d5d36533d
mahdee10 <120922767+mahdee10@users.noreply.github.com>
2025-05-03 22:04:30 +0200

    ignore warning as errors

--- src/auth/authProvider.js
+++ src/auth/authProvider.js
@@ -68,6 +68,7 @@ const AuthProvider = ({ children }) => {
             if (res) {
 
                 setUser(res.username);
+                console.log(res.token)
                 setToken(res.token);
                 
 
